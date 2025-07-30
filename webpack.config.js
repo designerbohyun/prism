d0d712407fbe92af286c3dbd38e29220c6d3430d
강민석 <kms860926@gmail.com>
2025-07-30 11:23:39 +0900

    webpack.config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,10 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [require("@tailwindcss/postcss"), require("autoprefixer")],
+                plugins: [
+                  require("@tailwindcss/postcss"),
+                  require("autoprefixer"),
+                ],
               },
             },
           },
